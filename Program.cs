8c351be1cb19df582759ca6f41eafbf198ceafa4
Дмитриев <dmitriev31827@gmail.com>
2026-05-12 22:28:37 +0300

    Дмитриев - добавлена главная форма Form1

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProductManager
+{
+    internal static class Program
+    {
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new CalendarForm());
+        }
+    }
+}
