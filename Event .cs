32655d2252457949023d23c3c198953a13742523
Дмитриев <dmitriev31827@gmail.com>
2026-05-12 22:21:23 +0300

    Дмитриев - добавлен класс Event

---
+++ Event .cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.IO;
+
+public class Event
+{
+    public DateTime Date { get; set; }
+    public string Description { get; set; }
+
+    public Event(DateTime date, string description)
+    {
+        Date = date;
+        Description = description;
+    }
+}
